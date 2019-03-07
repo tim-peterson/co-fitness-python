7987e528d24925a509a99158934a0de4f035e4a1
tim-peterson <petersontimr@gmail.com>
2019-03-07 10:41:05 -0600

    first file

---
+++ broad2019q1.py
@@ -0,0 +1,40 @@
+
+
+#from pydoc import help
+from scipy.stats.stats import pearsonr
+#help(pearsonr)
+
+import csv
+
+import numpy as np
+ 
+
+input = ["ATRAID"]
+
+with open('/Users/timrpeterson/OneDrive - Washington University in St. Louis/Data/Third Party/MORPHEOME/Hart-Moffat/qbf_Avanadata_2018.txt') as csv_file:
+	csv_reader = csv.reader(csv_file, delimiter='\t')
+	next(csv_reader)
+
+	with open('/Users/timrpeterson/OneDrive - Washington University in St. Louis/Data/Third Party/MORPHEOME/' + input[0] + 'pearsons-python.csv', 'wb') as csvfile:
+		spamwriter = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+	
+		genes = {}
+		for row in csv_reader:
+			gene = row[0]
+			row.pop(0)
+
+			#row_temp.pop(0)
+			genes[gene] = row
+
+		#for k, v in genes.iteritems():
+			#print k, v
+			#quit()
+
+		for key, value in genes.iteritems(): 
+
+			result = pearsonr(np.array(value).astype(np.float), np.array(genes[input[0]]).astype(np.float))
+
+			b = (key,) + result
+			spamwriter.writerow(b)
+
+
